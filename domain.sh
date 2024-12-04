52733ec38050d7038e07f0f76f3aecb08e2d814e
cdn25 <sushiltandipundlota@gmail.com>
2024-12-05 01:50:15 +0530

    Update domain.sh

--- domain.sh
+++ domain.sh
@@ -38,17 +38,27 @@ echo "Creating Nginx server block configuration file at $CONFIG_FILE..."
 sudo bash -c "cat > $CONFIG_FILE" <<EOL
 server {
     listen 80;
-    listen [::]:80;
-
     server_name $DOMAIN_NAME www.$DOMAIN_NAME;
 
     root $WEB_ROOT;
-	index index.php index.html index.htm;
+    index index.php index.html index.htm;
 
     location / {
         try_files \$uri \$uri/ =404;
     }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
 }
+
 EOL
 
 # Enable the configuration
